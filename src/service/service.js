bb46ab845d2dd92384f6a22a124683eec5f1517a
Di Coderr <dicoderr@gmail.com>
2020-08-14 21:40:55 +0300

    Add --version command

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+
+const DEFAULT_COMMAND = `--version`;
+const ExitCode = {success: 0, error: 1};
+
+const args = process.argv.slice(1);
+const [cmd] = args;
+
+if (args.length === 0 || !Cli[cmd]) {
+  Cli[DEFAULT_COMMAND].run();
+  process.exit(ExitCode.success);
+}
+
+Cli[cmd].run(args.slice(1));
