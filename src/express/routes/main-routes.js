d91f29813f3cdda865fd53b01cdf512381899be6
Di Coderr <dicoderr@gmail.com>
2021-01-15 09:05:49 +0200

    Add routes

--- src/express/routes/main-routes.js
+++ src/express/routes/main-routes.js
@@ -1,4 +1,19 @@
 'use strict';
 
-module.exports = () => {};
+const {Router} = require(`express`);
+const router = new Router();
 
+router.get(`/`, (req, res) => {
+  res.send(`/`);
+});
+router.get(`/register`, (req, res) => {
+  res.send(`/register`);
+});
+router.get(`/login`, (req, res) => {
+  res.send(`/login`);
+});
+router.get(`/search`, (req, res) => {
+  res.send(`/search`);
+});
+
+module.exports = router;
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -1,4 +1,13 @@
-
 'use strict';
 
-module.exports = () => {};
+const {Router} = require(`express`);
+const router = new Router();
+
+router.get(`/`, (req, res) => {
+  res.send(`/my`);
+});
+router.get(`/comments`, (req, res) => {
+  res.send(`/my/comments`);
+});
+
+module.exports = router;
